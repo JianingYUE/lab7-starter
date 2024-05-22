a98da57726a98e56dc4b12578473606f20f463ef
JianingYUE <58084890+JianingYUE@users.noreply.github.com>
2024-05-22 12:46:16 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -67,11 +67,8 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
-	const form = document.querySelector('new-recipe');
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
-	form.addEventListener('submit', (event) => {
-		event.preventDefault();
 	// Steps B4-B9 will occur inside the event listener from step B3
 	// B4. TODO - Create a new FormData object from the <form> element reference above
 	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
@@ -87,11 +84,16 @@ function initFormHandler() {
 	// Steps B12 & B13 will occur inside the event listener from step B11
 	// B12. TODO - Clear the local storage
 	// B13. TODO - Delete the contents of <main>
-	const formData = new FormData(form);
-    	const recipeObject = {};
-    	formData.forEach((value, key) => {
-    		recipeObject[key] = value;
-    	});
+    const form = document.getElementById('new-recipe');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const recipeObject = {};
+        formData.forEach((value, key) => {
+            recipeObject[key] = value;
+        });
 
         const recipeCard = document.createElement('recipe-card');
         recipeCard.data = recipeObject;
